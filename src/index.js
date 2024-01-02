f03ca9b7ce98cda107477af4c73e340ece03f9b8
UserFatimah <rihanatkhan12@gmail.com>
2024-01-02 09:36:27 -0800

    Version 3

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ const textElement = document.querySelector('.text');
 
 let timeoutId;
 buttonELement.addEventListener('click', function() {
-    // console.log(e);
     timeoutId = clearInterval();
-    // textElement.textContent = 'Happy New year';
     timeoutId(setInterval(() => {
         textElement.textContent = "Wishing  y'all a Happy New Year!!!";
     }, 1500))
